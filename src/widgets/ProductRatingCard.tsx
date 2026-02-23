1b588bce2fdc76f117719248eecdc32c9dba2bb5
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2026-02-23 13:26:21 -0600

    fix: add margin y on product card stars

--- src/widgets/ProductRatingCard.tsx
+++ src/widgets/ProductRatingCard.tsx
@@ -3,7 +3,7 @@ import { StarsRating } from "./components/StarsRating";
 
 export function ProductRatingCard() {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center my-2">
       <StarsRating count={3} bodyColor={widgetConfig.starBodyColor} fillColor={widgetConfig.starFillColor} emptyColor={widgetConfig.emptyStarColor} />
     </div>
   );
