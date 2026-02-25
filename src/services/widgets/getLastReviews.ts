d503c858d638f956debd6aa998b8515dbb5a1cf0
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2026-02-25 12:04:08 -0600

    fix: add fetch logs

--- src/services/widgets/getLastReviews.ts
+++ src/services/widgets/getLastReviews.ts
@@ -1,6 +1,9 @@
 import { widgetRequester } from "../widgetRequester";
 
 export async function getLastReviews(payload:GetLastReviewsPayload): Promise<GetLastReviewsResponse> {
+
+  console.log("fetching last Reviews");
+
   return widgetRequester({
     method: "POST",
     endpoint: "/widget/lastreviews",
--- src/services/widgets/getProductReviews.ts
+++ src/services/widgets/getProductReviews.ts
@@ -1,6 +1,8 @@
 import { widgetRequester } from "../widgetRequester";
 
 export async function getProductReviews(payload:GetProductReviewsPayload): Promise<GetProductReviewsResponse> {
+
+  console.log("fetching product Reviews");
   return widgetRequester({
     method: "POST",
     endpoint: "/widget/product/reviews",
